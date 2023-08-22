cf8bc19500c0029b44cbfec6c1bd3daf82a621c0
Mustafa Salman <frostnote2@gmail.com>
2023-08-22 18:50:20 +0800

    Uncommented the test function for the convert function

--- function.py
+++ function.py
@@ -11,4 +11,4 @@ def multiply(a, b):
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9/ 5) # <--- fix this in step 7
+    return multiply(subtract(fahrenheit, 32), 9 / 5)  # <--- fix this in step 7
